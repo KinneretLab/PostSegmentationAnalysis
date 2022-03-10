e5d78fa5d96cc97a6c2e44ec4417cb5254cd0168
Noam Dori <TheNODO55@gmail.com>
2022-03-10 07:47:29 +0200

    added various visual options for the score indicator images

--- mainScoreImages.m
+++ mainScoreImages.m
@@ -5,7 +5,12 @@ addpath(genpath('\\phhydra\phhydraB\Analysis\users\Projects\Noam'));
 mainDir='Z:\Analysis\users\Projects\Noam\Workshop\\timepoints'; % Main directory for movie you are analysing.
 cellDir = [mainDir,'\Cells\']; % Cell directory for movie (this is our normal folder structure and should stay consistent).
 segDir = [cellDir,'Inference\2022_01_11_CEE3_CEE5_CEE1E_CEE1E_CEE6']; % Segmentation folder.
+
+% various visual configurations for the result images
 isBinary = false; % whether binary score images should be saved or a more continuous variant with a variable color.
+darken = false; % whether the cell coloring should be darked overall so the raw image can be more visible
+erodeCells = false; % whether there should be a buffer space between the indicators of each cell
+showBorders = true; % whether the automatic segmentatio borders should be shown (in yellow)
 
 cd(cellDir);
 load('fullCellDataMod');
@@ -22,22 +27,25 @@ for i = 1:length(subDirNames)
     % get all the images
     wireframe = imread(fullfile(segDir, dirName, 'handCorrection.tif'));
     loadedFrames{i, 1} = wireframe(:, :, 1);
-    loadedFrames{i, 2} = loadedFrames{i, 1};
-    loadedFrames{i, 3} = imread(fullfile(cellDir, 'Raw Cortices', dirName + ".tiff"));
+    loadedFrames{i, 2} = imread(fullfile(cellDir, 'Raw Cortices', dirName + ".tiff"));
 end
 
-lenImages = size(loadedFrames, 2);
-
-summaryImages = loadedFrames(:, 1);
+imgSize = size(loadedFrames{1, 1})
+if showBorders
+    summaryImages = loadedFrames(:, 1);
+else
+    summaryImages = {zeros(imgSize), zeros(imgSize)};
+end
 
 disp('Creating composite images...');
 for cellIdx = 1:length(fullCellDataMod)
     cellData = fullCellDataMod(cellIdx);
     images = loadedFrames(str2double(cellData.frame) + 1, :);
 
-    [fakeImg, cellImg] = fillScore(cellData, images{1:2});
+    [fakeImg, cellImg] = fillScore(cellData, images{1});
     summaryImages{str2double(cellData.frame) + 1} = summaryImages{str2double(cellData.frame) + 1} + ...
-       getWeight(isBinary, cellData.confidence) * (255 * cat(3, fakeImg, cellImg, zeros(size(fakeImg))));
+       getWeight(isBinary, cellData.confidence, darken) * 255 * cat(3, tryErode(fakeImg, erodeCells), ...
+       tryErode(cellImg, erodeCells), zeros(imgSize));
 
     if rem(cellIdx, 1000) == 0
         disp(num2str(cellIdx))
@@ -48,15 +56,16 @@ histogram([fullCellDataMod.confidence], 'BinWidth', 0.01);
 
 disp('Saving images...');
 for imgIdx = 1:length(subDirs)
-    sumImg = cat(3, summaryImages{imgIdx}(:,:,1), summaryImages{imgIdx}(:,:,2), im2uint8(loadedFrames{imgIdx, 3}(:,:,1)));
+    sumImg = cat(3, summaryImages{imgIdx}(:,:,1), summaryImages{imgIdx}(:,:,2), im2uint8(loadedFrames{imgIdx, 2}(:,:,1)));
     saveToFolder(cellDir, sumImg, subDirNames(imgIdx));
 end
 
-function [fakeImg, cellImg] = fillScore(cellData, fakeImg, cellImg)
+function [fakeImg, cellImg] = fillScore(cellData, segImg)
+    rawMask = 255 * uint8(imfill(imbinarize(segImg(:,:,1)), double(cellData.outline(1,:))) - imbinarize(segImg(:,:,1)));
     if cellData.confidence >= 0.5
-        cellImg = 255 * uint8(imfill(imbinarize(cellImg(:,:,1)), double(cellData.outline(1,:))) - imbinarize(cellImg(:,:,1)));
+        cellImg = rawMask;
     else
-        fakeImg = 255 * uint8(imfill(imbinarize(fakeImg(:,:,1)), double(cellData.outline(1,:))) - imbinarize(fakeImg(:,:,1)));
+        fakeImg = rawMask;
     end
 end
 
@@ -68,10 +77,19 @@ function saveToFolder(location, images, imgID)
     imwrite(images, location + subFolder + imgID + ".tif");
 end
 
-function weight = getWeight(binary, score)
-    if binary == 1
+function weight = getWeight(binary, score, doDark)
+    if binary
         weight = 1;
     else
         weight = 1.8 * abs(score - 0.5) + 0.2;
+    end
+    if doDark
+        weight = weight * 0.2;
+    end
+end
+
+function img = tryErode(img, erode)
+    if erode
+        img = imerode(img, strel('disk', 3))
     end
 end
