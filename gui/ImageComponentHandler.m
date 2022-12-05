767d3617425040ecde70cec9537b958dd82a557a
Noam Dori <theNODO55@gmail.com>
2022-12-05 13:41:42 +0200

    added new gui for new version of image builder

--- gui/ImageComponentHandler.m
+++ gui/ImageComponentHandler.m
@@ -13,6 +13,12 @@ classdef ImageComponentHandler < handle
         figures_
         layers_panel_
     end
+
+    properties (Constant)
+        image_type="image";
+        marker_type="list";
+        quiver_type="quiver";
+    end
     
     methods(Access=public)
         function obj= ImageComponentHandler(imageDisplayHandler, imageBuilder, app)
@@ -21,28 +27,26 @@ classdef ImageComponentHandler < handle
             obj.app_=app;
             obj.layer_data_panel_=LayerDataPanel(obj.app_.GridLayout3, obj);
             obj.layers_panel_=LayersPanel(app.GridLayout8, obj);
-
-
         end
         
-        function show(obj, app)
-            obj.renderFromImageBuilder(app);
-            app.ImageTab.Parent=app.TabGroup;
-            obj.showLayers(app);
-            obj.setImageSettings(app);
+        function show(obj)
+            obj.renderFromImageBuilder(obj.app_);
+            obj.app_.ImageTab.Parent=obj.app_.TabGroup;
+            obj.showLayers(obj.app_);
+            obj.setImageSettings(obj.app_);
         end
         
         function saveFigures(obj)
-            [file, path] = uiputfile("*.*");
-            if(file ==0)
+            [~, path] = uiputfile("*.*");
+            if(path ==0)
                 return;
-            end          
-            obj.ImageBuilder.save(obj.figures_, path, file);
+            end
+            obj.ImageBuilder.output_folder(path).draw();
         end
         
         function renderFromInput(obj, app)
             obj.setImageData(app);
-            obj.ImageBuilder.layers_data_{obj.shown_layer_}=obj.layer_data_panel_.getLayerData(obj.ImageBuilder.layers_data_{obj.shown_layer_});
+            obj.ImageBuilder.layers_data(obj.shown_layer_,obj.layer_data_panel_.getLayerData(obj.ImageBuilder.layers_data(obj.shown_layer_)))
             obj.renderFromImageBuilder(app);
             if(~obj.canDeleteBackground())
                 obj.app_.DeleteBackgroundButton.Enable=false;
@@ -51,12 +55,10 @@ classdef ImageComponentHandler < handle
         
         function renderFromImageBuilder(obj, app)
             obj.is_layer_first_loaded_=false;
-            obj.ImageDisplayHandler.forceCloseFigures();
-            obj.figures_=obj.ImageBuilder.draw(false);
-            obj.ImageDisplayHandler.setFigures(obj.figures_);
-            obj.ImageDisplayHandler.show(app);
+            obj.ImageDisplayHandler.closeFigure;
+            obj.ImageDisplayHandler.show;
             app.ChooseFrameSlider.Value=obj.frame_default_value_;
-            obj.setNumOfFrames(app, obj.figures_);
+            obj.setNumOfFrames(app, length(obj.ImageBuilder.layer_arr));
         end
         
         function loadBackground(obj)
@@ -69,7 +71,7 @@ classdef ImageComponentHandler < handle
         
         function changeLayer(obj, layer_num)
             if(obj.is_layer_first_loaded_)
-                obj.ImageBuilder.layers_data_{obj.shown_layer_}=obj.layer_data_panel_.getLayerData(obj.ImageBuilder.layers_data_{obj.shown_layer_});
+                obj.ImageBuilder.layers_data(obj.shown_layer_, obj.layer_data_panel_.getLayerData(obj.ImageBuilder.layers_data(obj.shown_layer_)));
             else
                 obj.is_layer_first_loaded_=true;
             end
@@ -84,41 +86,40 @@ classdef ImageComponentHandler < handle
     end
     
     methods (Access=private)     
-        function setNumOfFrames(~, app, figures)
-            [~, col]=size(figures);
-            if(col==1)
+        function setNumOfFrames(~, app, num)
+            if(num==1)
                 app.ChooseFrameSlider.Visible=false;
                 return;
             end
             app.ChooseFrameSlider.Visible=true;
-            app.ChooseFrameSlider.Limits= [1 col];
+            app.ChooseFrameSlider.Limits= [1 num];
         end
         
         function setImageData(obj, app)
-            obj.ImageBuilder.image_data_.setImageTitle(app.ImageTitleEditField.Value);
+            obj.ImageBuilder.image_data.setImageTitle(app.ImageTitleEditField.Value);
             color = [app.RValue.Value app.GValue.Value app.BValue.Value];
-            obj.ImageBuilder.image_data_.setColorForNaN(color);
-            obj.ImageBuilder.image_data_.setShowColorbar(app.ShowColorbarCheckBox.Value);
-            obj.ImageBuilder.image_data_.setColorbarTitle(app.ColorbarTitleEditField.Value);
+            obj.ImageBuilder.image_data.setColorForNaN(color);
+            obj.ImageBuilder.image_data.setShowColorbar(app.ShowColorbarCheckBox.Value);
+            obj.ImageBuilder.image_data.setColorbarTitle(app.ColorbarTitleEditField.Value);
             scale=[app.ColorbarAxisMin.Value app.ColorbarAxisMax.Value];
-            obj.ImageBuilder.image_data_.setColorbarAxisScale(scale);
-            obj.ImageBuilder.image_data_.setLegendForMarkers(app.ShowLegendCheckBox.Value);
+            obj.ImageBuilder.image_data.setColorbarAxisScale(scale);
+            obj.ImageBuilder.image_data.setLegendForMarkers(app.ShowLegendCheckBox.Value);
         end
         
         function setImageSettings(obj, ~)
-            obj.app_.ImageTitleEditField.Value=obj.ImageBuilder.image_data_.getImageTitle();
-            color=obj.ImageBuilder.image_data_.getColorForNaN();
+            obj.app_.ImageTitleEditField.Value=obj.ImageBuilder.image_data.getImageTitle();
+            color=obj.ImageBuilder.image_data.getColorForNaN();
             obj.app_.RValue.Value=color(1);
             obj.app_.GValue.Value=color(2);
             obj.app_.BValue.Value=color(3);
-            obj.app_.ShowColorbarCheckBox.Value =obj.ImageBuilder.image_data_.getShowColorbar();
-            obj.app_.ColorbarTitleEditField.Value=obj.ImageBuilder.image_data_.getColorbarTitle();
-            colorbar_scale=obj.ImageBuilder.image_data_.getColorbarAxisScale();
+            obj.app_.ShowColorbarCheckBox.Value =obj.ImageBuilder.image_data.getShowColorbar();
+            obj.app_.ColorbarTitleEditField.Value=obj.ImageBuilder.image_data.getColorbarTitle();
+            colorbar_scale=obj.ImageBuilder.image_data.getColorbarAxisScale();
             if(~isempty(colorbar_scale))
                 obj.app_.ColorbarAxisMin.Value=colorbar_scale(1);
                 obj.app_.ColorbarAxisMax.Value=colorbar_scale(2);
             end
-            obj.app_.ShowLegendCheckBox.Value=obj.ImageBuilder.image_data_.getLegendForMarkers();
+            obj.app_.ShowLegendCheckBox.Value=obj.ImageBuilder.image_data.getLegendForMarkers();
             obj.app_.DeleteBackgroundButton.Enable=true;
             if(~obj.canDeleteBackground())
                 obj.app_.DeleteBackgroundButton.Enable=false;
@@ -126,13 +127,13 @@ classdef ImageComponentHandler < handle
         end
         
         function can_delete= canDeleteBackground(obj)
-            [~, col]=size(obj.ImageBuilder.layers_data_);
-            if(isempty(obj.ImageBuilder.image_data_.getBackgroundImage()))
+            [~, col]=size(obj.ImageBuilder.layers_data);
+            if(isempty(obj.ImageBuilder.image_data.getBackgroundImage()))
                 can_delete=false;
                 return;
             end
             for i=1:col
-                can_delete=~obj.ImageBuilder.layers_data_{i}.getIsMarkerLayer() && ~obj.ImageBuilder.layers_data_{i}.getIsMarkerQuiver();
+                can_delete=(~obj.ImageBuilder.layers_data(i).getType()==obj.image_type);
                 if(can_delete)
                     return;
                 end
@@ -140,11 +141,11 @@ classdef ImageComponentHandler < handle
         end
         
         function showLayers(obj, ~)
-            obj.layers_panel_.create(obj.ImageBuilder.layers_data_);
+            obj.layers_panel_.create(obj.ImageBuilder.layers_data);
         end 
         
         function showLayerData(obj)
-            obj.layer_data_panel_.create(obj.ImageBuilder.layers_data_{obj.shown_layer_})
+            obj.layer_data_panel_.create(obj.ImageBuilder.layers_data(obj.shown_layer_))
         end 
     end
 end
