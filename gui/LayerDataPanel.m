e94d0179976fb90fd95e2bad98b87a37258efd50
Iris Pasvinter <iris.p.98@gmail.com>
2022-12-12 15:05:00 +0200

    added line width to gui and class name

--- gui/LayerDataPanel.m
+++ gui/LayerDataPanel.m
@@ -1,11 +1,12 @@
 classdef LayerDataPanel<handle
     %UNTITLED Summary of this class goes here
     %   Detailed explanation goes here
-    
+
     properties
         grid_
         image_component_handler_
         ShowCheckBox
+        ColorbarCheckBox
         ColormapDropDownLabel
         ColormapDropDown
         ScaleGrid
@@ -34,7 +35,7 @@ classdef LayerDataPanel<handle
         LineWidthSpinnerLabel
         LineWidthSpinner
     end
-    
+
     methods
         function obj = LayerDataPanel(grid, image_component_handler)
             obj.grid_=grid;
@@ -42,10 +43,11 @@ classdef LayerDataPanel<handle
             obj.createData();
             obj.hide();
         end
-        
+
         function create(obj, layer_data)
             obj.hide();
             type=layer_data.getType();
+            obj.setClass(layer_data);
             switch(type)
                 case obj.image_component_handler_.image_type
                     obj.showImageLayerPanel();
@@ -58,200 +60,213 @@ classdef LayerDataPanel<handle
                     obj.setQuiverLayerSettings(layer_data);
             end
         end
-        
+
         function hide(obj)
             obj.hideImageLayerPanel();
             obj.hideMarkerLayerPanel();
             obj.hideSharedLayerPanel();
             obj.hideQuiverLayerPanel();
         end
-        
+
         function showMarkerLayerPanel(obj)
-            obj.grid_.RowHeight = { '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x'};
-            obj.Type.Text = 'Marker';
+            obj.grid_.RowHeight = { '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x', '1x'};
             obj.ShowCheckBox.Parent=obj.grid_;
             obj.ShowCheckBox.Layout.Row = 2;
             obj.ShowCheckBox.Layout.Column = 1;
-            
+
+            obj.ColorbarCheckBox.Parent=obj.grid_;
+            obj.ColorbarCheckBox.Layout.Row = 2;
+            obj.ColorbarCheckBox.Layout.Column = 2;
+
             obj.ColormapDropDownLabel.Parent=obj.grid_;
             obj.ColormapDropDownLabel.Layout.Row = 3;
             obj.ColormapDropDownLabel.Layout.Column = 1;
-            
+
             obj.ColormapDropDown.Parent=obj.grid_;
             obj.ColormapDropDown.Layout.Row = 3;
             obj.ColormapDropDown.Layout.Column = 2;
-            
+
             obj.OpacitySpinnerLabel.Parent = obj.grid_;
             obj.OpacitySpinnerLabel.Layout.Row = 4;
             obj.OpacitySpinnerLabel.Layout.Column = 1;
-            
+
             obj.OpacitySpinner.Parent = obj.grid_;
             obj.OpacitySpinner.Layout.Row = 4;
             obj.OpacitySpinner.Layout.Column = 2;
-            
+
             obj.ShapeDropDownLabel.Parent = obj.grid_;
             obj.ShapeDropDownLabel.Layout.Row = 5;
             obj.ShapeDropDownLabel.Layout.Column = 1;
-            
+
             obj.ShapeDropDown.Parent = obj.grid_;
             obj.ShapeDropDown.Layout.Row = 5;
             obj.ShapeDropDown.Layout.Column = 2;
-            
+
             obj.ColorbyValueCheckBox.Parent = obj.grid_;
             obj.ColorbyValueCheckBox.Layout.Row = 6;
             obj.ColorbyValueCheckBox.Layout.Column = 1;
-            
+
             obj.ColorDropDownLabel.Parent = obj.grid_;
             obj.ColorDropDownLabel.Layout.Row = 7;
             obj.ColorDropDownLabel.Layout.Column = 1;
-            
+
             obj.ColorDropDown.Parent = obj.grid_;
             obj.ColorDropDown.Layout.Row = 7;
             obj.ColorDropDown.Layout.Column = 2;
-            
+
             obj.SizeByValueCheckBox.Parent = obj.grid_;
             obj.SizeByValueCheckBox.Layout.Row = 6;
             obj.SizeByValueCheckBox.Layout.Column = 2;
-            
+
             obj.SizeSpinnerLabel.Parent = obj.grid_;
             obj.SizeSpinnerLabel.Layout.Row = 8;
             obj.SizeSpinnerLabel.Layout.Column = 1;
-            
+
             obj.SizeSpinner.Parent = obj.grid_;
             obj.SizeSpinner.Layout.Row = 8;
             obj.SizeSpinner.Layout.Column = 2;
-            
+
             obj.TypeLabel.Parent = obj.grid_;
             obj.TypeLabel.Layout.Row = 1;
             obj.TypeLabel.Layout.Column = 1;
-            
+
             obj.Type.Parent = obj.grid_;
             obj.Type.Layout.Row = 1;
             obj.Type.Layout.Column = 2;
+
+            obj.LineWidthSpinnerLabel.Parent=obj.grid_;
+            obj.LineWidthSpinnerLabel.Layout.Row=9;
+            obj.LineWidthSpinnerLabel.Layout.Column=1;
+
+            obj.LineWidthSpinner.Parent=obj.grid_;
+            obj.LineWidthSpinner.Layout.Row=9;
+            obj.LineWidthSpinner.Layout.Column=2;
         end
-        
+
         function showImageLayerPanel(obj)
             obj.grid_.RowHeight={'1x', '1x', '1x', '1x', '1x', '1x', '1x'};
-            obj.Type.Text = 'Image';
             obj.ShowCheckBox.Parent=obj.grid_;
             obj.ShowCheckBox.Layout.Row = 2;
             obj.ShowCheckBox.Layout.Column = 1;
-            
+
+            obj.ColorbarCheckBox.Parent=obj.grid_;
+            obj.ColorbarCheckBox.Layout.Row = 2;
+            obj.ColorbarCheckBox.Layout.Column = 2;
+
             obj.ColormapDropDownLabel.Parent=obj.grid_;
             obj.ColormapDropDownLabel.Layout.Row = 4;
             obj.ColormapDropDownLabel.Layout.Column = 1;
-            
+
             obj.ColormapDropDown.Parent=obj.grid_;
             obj.ColormapDropDown.Layout.Row = 4;
             obj.ColormapDropDown.Layout.Column = 2;
-            
+
             obj.ScaleGrid.Parent=obj.grid_;
             obj.ScaleGrid.Layout.Row = 3;
             obj.ScaleGrid.Layout.Column = 2;
-            
+
             obj.ScaleMin.Parent=obj.ScaleGrid;
             obj.ScaleMin.Layout.Row = 1;
             obj.ScaleMin.Layout.Column = 1;
-            
+
             obj.ScaleMax.Parent=obj.ScaleGrid;
             obj.ScaleMax.Layout.Row = 1;
             obj.ScaleMax.Layout.Column = 2;
-            
+
             obj.ScaleLabel.Parent = obj.grid_;
             obj.ScaleLabel.Layout.Row = 3;
             obj.ScaleLabel.Layout.Column = 1;
-            
+
             obj.ColorLabel.Parent = obj.grid_;
             obj.ColorLabel.Layout.Row = 6;
             obj.ColorLabel.Layout.Column = 1;
-            
+
             obj.FillCheckBox.Parent = obj.grid_;
             obj.FillCheckBox.Layout.Row = 5;
             obj.FillCheckBox.Layout.Column = [1 2];
-            
+
             obj.ColorGrid.Parent = obj.grid_;
             obj.ColorGrid.Layout.Row = 6;
             obj.ColorGrid.Layout.Column = 2;
-            
+
             obj.RValue.Parent = obj.ColorGrid;
             obj.RValue.Layout.Row = 1;
             obj.RValue.Layout.Column = 1;
-            
+
             obj.GValue.Parent = obj.ColorGrid;
             obj.GValue.Layout.Row = 1;
             obj.GValue.Layout.Column = 2;
-            
+
             obj.BValue.Parent = obj.ColorGrid;
             obj.BValue.Layout.Row = 1;
             obj.BValue.Layout.Column = 3;
-            
+
             obj.OpacitySpinnerLabel.Parent = obj.grid_;
             obj.OpacitySpinnerLabel.Layout.Row = 7;
             obj.OpacitySpinnerLabel.Layout.Column = 1;
-            
+
             obj.OpacitySpinner.Parent = obj.grid_;
             obj.OpacitySpinner.Layout.Row = 7;
             obj.OpacitySpinner.Layout.Column = 2;
-            
+
             obj.TypeLabel.Parent = obj.grid_;
             obj.TypeLabel.Layout.Row = 1;
             obj.TypeLabel.Layout.Column = 1;
-            
+
             obj.Type.Parent = obj.grid_;
             obj.Type.Layout.Row = 1;
             obj.Type.Layout.Column = 2;
         end
-        
+
         function showQuiverLayerPanel(obj)
-            obj.Type.Text = 'Quiver';
             obj.ShowCheckBox.Parent=obj.grid_;
             obj.ShowCheckBox.Layout.Row = 2;
             obj.ShowCheckBox.Layout.Column = 1;
-            
+
             obj.ColorDropDownLabel.Parent = obj.grid_;
             obj.ColorDropDownLabel.Layout.Row = 3;
             obj.ColorDropDownLabel.Layout.Column = 1;
-            
+
             obj.ColorDropDown.Parent = obj.grid_;
             obj.ColorDropDown.Layout.Row = 3;
             obj.ColorDropDown.Layout.Column = 2;
-            
+
             obj.SizeByValueCheckBox.Parent = obj.grid_;
             obj.SizeByValueCheckBox.Layout.Row = 4;
             obj.SizeByValueCheckBox.Layout.Column = 1;
-            
+
             obj.TypeLabel.Parent = obj.grid_;
             obj.TypeLabel.Layout.Row = 1;
             obj.TypeLabel.Layout.Column = 1;
-            
+
             obj.Type.Parent = obj.grid_;
             obj.Type.Layout.Row = 1;
             obj.Type.Layout.Column = 2;
-            
+
             obj.SizeSpinnerLabel.Parent = obj.grid_;
             obj.SizeSpinnerLabel.Layout.Row = 5;
             obj.SizeSpinnerLabel.Layout.Column = 1;
-            
+
             obj.SizeSpinner.Parent = obj.grid_;
             obj.SizeSpinner.Layout.Row = 5;
             obj.SizeSpinner.Layout.Column = 2;
-            
+
             obj.ShowArowheadCheckBox.Parent=obj.grid_;
             obj.ShowArowheadCheckBox.Layout.Row=4;
             obj.ShowArowheadCheckBox.Layout.Column=2;
-            
+
             obj.LineWidthSpinnerLabel.Parent=obj.grid_;
             obj.LineWidthSpinnerLabel.Layout.Row=6;
             obj.LineWidthSpinnerLabel.Layout.Column=1;
-            
+
             obj.LineWidthSpinner.Parent=obj.grid_;
             obj.LineWidthSpinner.Layout.Row=6;
             obj.LineWidthSpinner.Layout.Column=2;
 
-            
-            
+
+
         end
-        
+
         function hideImageLayerPanel(obj)
             obj.ScaleGrid.Parent=[];
             obj.ScaleMin.Parent=[];
@@ -264,7 +279,7 @@ classdef LayerDataPanel<handle
             obj.GValue.Parent = [];
             obj.BValue.Parent = [];
         end
-        
+
         function hideMarkerLayerPanel(obj)
             obj.ShapeDropDownLabel.Parent = [];
             obj.ShapeDropDown.Parent = [];
@@ -274,34 +289,37 @@ classdef LayerDataPanel<handle
             obj.SizeByValueCheckBox.Parent = [];
             obj.SizeSpinnerLabel.Parent = [];
             obj.SizeSpinner.Parent = [];
+
         end
-        
+
         function hideQuiverLayerPanel(obj)
             obj.ShowArowheadCheckBox.Parent=[];
-            obj.LineWidthSpinnerLabel.Parent=[];
-            obj.LineWidthSpinner.Parent=[];
         end
-        
+
         function hideSharedLayerPanel(obj)
             obj.ShowCheckBox.Parent=[];
+            obj.ColorbarCheckBox.Parent=[];
             obj.ColormapDropDownLabel.Parent=[];
             obj.ColormapDropDown.Parent=[];
             obj.OpacitySpinnerLabel.Parent = [];
             obj.OpacitySpinner.Parent = [];
             obj.TypeLabel.Parent = [];
             obj.Type.Parent = [];
+            obj.LineWidthSpinnerLabel.Parent=[];
+            obj.LineWidthSpinner.Parent=[];
         end
-        
-        
+
+
         function createData(obj)
             obj.createSharedLayerData();
             obj.createImageLayerData();
             obj.createMarkerLayerData();
             obj.createQuiverLayerData();
         end
-        
+
         function createSharedLayerData(obj)
             obj.ShowCheckBox=uicheckbox(obj.grid_);
+            obj.ColorbarCheckBox=uicheckbox(obj.grid_);
             obj.ColormapDropDownLabel=uilabel(obj.grid_);
             obj.ColormapDropDown = uidropdown(obj.grid_);
             obj.ColormapDropDown.Items=PresetValues.getColormaps;
@@ -317,6 +335,7 @@ classdef LayerDataPanel<handle
             obj.SizeSpinner.Step =0.1;
             obj.SizeSpinner.Limits =[0 Inf];
             obj.ShowCheckBox.Text = 'Show';
+            obj.ColorbarCheckBox.Text = 'Colorbar for Layer';
             obj.ColormapDropDownLabel.Text = 'Colormap';
             obj.OpacitySpinnerLabel.Text = 'Opacity';
             obj.OpacitySpinner.Limits = [0 1];
@@ -325,9 +344,11 @@ classdef LayerDataPanel<handle
             obj.ColorDropDown.Items=PresetValues.getColors;
             obj.SizeByValueCheckBox.Text = 'Size By Value';
             obj.SizeSpinnerLabel.Text = 'Size';
-            
+            obj.LineWidthSpinnerLabel= uilabel(obj.grid_);
+            obj.LineWidthSpinnerLabel.Text="Line Width";
+            obj.LineWidthSpinner=uispinner(obj.grid_);
         end
-        
+
         function createImageLayerData(obj)
             obj.ScaleGrid = uigridlayout(obj.grid_);
             obj.ScaleMin = uieditfield(obj.ScaleGrid, 'numeric');
@@ -352,7 +373,7 @@ classdef LayerDataPanel<handle
             obj.BValue.Step = 0.1;
             obj.BValue.Limits = [0 1];
         end
-        
+
         function createMarkerLayerData(obj)
             obj.ShapeDropDownLabel = uilabel(obj.grid_);
             obj.ShapeDropDown = uidropdown(obj.grid_);
@@ -362,28 +383,28 @@ classdef LayerDataPanel<handle
             obj.ColorDropDownLabel.Text = 'Color';
             obj.ShapeDropDown.Items=PresetValues.getMarkerShapes;
         end
-        
+
         function createQuiverLayerData(obj)
             obj.ShowArowheadCheckBox= uicheckbox(obj.grid_);
             obj.ShowArowheadCheckBox.Text = 'Show Arrowhead';
-
-            obj.LineWidthSpinnerLabel= uilabel(obj.grid_);
-            obj.LineWidthSpinnerLabel.Text="Line Width";
-            obj.LineWidthSpinner=uispinner(obj.grid_);
         end
-        
+
+        function setClass(obj, layer_data)
+            obj.Type.Text = layer_data.getClass;
+        end
+
         function setQuiverLayerSettings(obj, layer_data)
             obj.ShowCheckBox.Value=layer_data.getShow();
             obj.ColorDropDown.Value=layer_data.getMarkersColor();
             obj.SizeByValueCheckBox.Value= layer_data.getMarkersSizeByValue();
             obj.ShowArowheadCheckBox.Value= layer_data.getQuiverShowArrowHead();
             obj.SizeSpinner.Value=layer_data.getMarkersSize();
-            lin=layer_data.getQuiverLineWidth();
-            obj.LineWidthSpinner.Value=layer_data.getQuiverLineWidth();
+            obj.LineWidthSpinner.Value=layer_data.getLineWidth();
         end
-        
+
         function setImageLayerSettings(obj, layer_data)
             obj.ShowCheckBox.Value=layer_data.getShow();
+            obj.ColorbarCheckBox.Value=layer_data.getColorbar();
             scale=layer_data.getScale();
             obj.ScaleMax.Value=scale(2);
             obj.ScaleMin.Value=scale(1);
@@ -395,9 +416,10 @@ classdef LayerDataPanel<handle
             obj.OpacitySpinner.Value=layer_data.getOpacity();
             obj.ColormapDropDown.Value=layer_data.getColormap();
         end
-        
+
         function setMarkerLayerSettings(obj, layer_data)
             obj.ShowCheckBox.Value=layer_data.getShow();
+            obj.ColorbarCheckBox.Value=layer_data.getColorbar();
             obj.OpacitySpinner.Value=layer_data.getOpacity();
             obj.ColorbyValueCheckBox.Value = layer_data.getMarkersColorByValue();
             obj.SizeByValueCheckBox.Value= layer_data.getMarkersSizeByValue();
@@ -405,8 +427,9 @@ classdef LayerDataPanel<handle
             obj.ColormapDropDown.Value=layer_data.getColormap();
             obj.ShapeDropDown.Value=layer_data.getMarkersShape();
             obj.ColorDropDown.Value=layer_data.getMarkersColor();
+            obj.LineWidthSpinner.Value=layer_data.getLineWidth();
         end
-        
+
         function layer_data =getLayerData(obj, layer_data)
             type=layer_data.getType;
             switch(type)
@@ -419,18 +442,20 @@ classdef LayerDataPanel<handle
                     layer_data=obj.getQuiverLayerSettings(layer_data);
             end
         end
-        
+
         function layer_data = getImageLayerSettings(obj, layer_data)
             layer_data.setShow(obj.ShowCheckBox.Value);
+            layer_data.setColorbar(obj.ColorbarCheckBox.Value);
             layer_data.setScale([obj.ScaleMin.Value obj.ScaleMax.Value]);
             layer_data.setIsSolidColor(obj.FillCheckBox.Value);
             layer_data.setSolidColor([obj.RValue.Value obj.GValue.Value obj.BValue.Value] );
             layer_data.setOpacity(obj.OpacitySpinner.Value);
             layer_data.setColormap(obj.ColormapDropDown.Value);
         end
-        
+
         function layer_data= getMarkerLayerSettings(obj, layer_data)
             layer_data.setShow(obj.ShowCheckBox.Value);
+            layer_data.setColorbar(obj.ColorbarCheckBox.Value);
             layer_data.setOpacity(obj.OpacitySpinner.Value);
             layer_data.setMarkersColorByValue(obj.ColorbyValueCheckBox.Value);
             layer_data.setMarkersSizeByValue(obj.SizeByValueCheckBox.Value);
@@ -440,15 +465,17 @@ classdef LayerDataPanel<handle
             layer_data.setColormap(obj.ColormapDropDown.Value);
             layer_data.setMarkersColor(obj.ColorDropDown.Value);
             layer_data.setMarkersShape(obj.ShapeDropDown.Value);
+            layer_data.setLineWidth(obj.LineWidthSpinner.Value);
+
         end
-        
+
         function layer_data=getQuiverLayerSettings(obj, layer_data)
             layer_data.setShow(obj.ShowCheckBox.Value);
             layer_data.setMarkersColor(obj.ColorDropDown.Value);
             layer_data.setMarkersSizeByValue(obj.SizeByValueCheckBox.Value);
             layer_data.setQuiverShowArrowHead(obj.ShowArowheadCheckBox.Value);
             layer_data.setMarkersSize(obj.SizeSpinner.Value);
-            layer_data.setQuiverLineWidth(obj.LineWidthSpinner.Value);
+            layer_data.setLineWidth(obj.LineWidthSpinner.Value);
         end
     end
 end
